34ed565b4f24bd937e8646f35f8e4cc71245142a
Macaiyla Steadman <msteadman@ggc.edu>
2026-03-05 23:23:55 -0500

    Changed my name format

--- TeamSuperHero.java
+++ TeamSuperHero.java
@@ -2,7 +2,7 @@ package teamSuperHero;
 
 public class TeamSuperHero {
     public static void main(String[] args) {
-        System.out.print("My name is Macaiyla");
+        System.out.print("Macaiyla Steadman");
     }
 
     public static void main(String[] args) {
